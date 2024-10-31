e7663a095b4f6b7c42276bd5393b8288c07e1e8b
thislife_won <sw8744@naver.com>
2024-10-31 10:53:21 +0900

    Edit host

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def read_root(grade, class_):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=7000)
+    uvicorn.run(app, host="0.0.0.0/0", port=7000)
